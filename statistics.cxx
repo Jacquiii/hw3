622d44528d73ca2a5eed81b943760b4ef3ae1393
Jacqueline Fedyk <jafed100@pc09.pool.tp1.uni-duesseldorf.de>
2015-11-12 12:38:46 +0100

    homework3

--- statistics.cxx
+++ statistics.cxx
@@ -3,16 +3,41 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
 
+void random(double* p, const int N)
+{
+ for(int i=0; i<N; i++){
+    double r = rand();
+    p[i] = r / RAND_MAX;
+   }
+}
+     
+void calculation(double*p,const int N,double& mean,double& var)
+{
+
+ double sum1=0; 
+ for(int i=0; i<N; i++){
+    sum1 += p[i];
+    }
+    mean=(sum1)/(N); 
+
+    
+ double sum2=0;
+ for(int i=0; i<N; i++){
+    sum2 +=(p[i]-mean)*(p[i]-mean); 
+    }
+    var=(sum2)/(N);
+
+} 
+ 
 int main(){
    const int N = 100;
    double p[N];
    double mean, var;
+   
+random(p,N);
+calculation(p,N,mean,var);
 
-   // Some lines here....
 
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
